4826f30f91ecbc1e32f044107148136b9434ff57
Taylor Stigen <106631704+TaylorStigen@users.noreply.github.com>
2024-10-20 19:28:39 -0600

    Add local Bootstrap
    Implemented custom coloring

--- _config.ts
+++ _config.ts
@@ -1,10 +1,12 @@
 import lume from "lume/mod.ts";
+import sass from "lume/plugins/sass.ts";
 
 const site = lume();
 
 site.copy("images");
+//site.copy("styles");
 site.ignore("README.md");
 site.data("currentDate", new Date().getFullYear());
-site.filter("uppercase", (value) => value.toUpperCase());
+site.use(sass());
 
 export default site;
